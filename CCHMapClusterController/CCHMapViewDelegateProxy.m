dc71c9a03ce95004211a9768252bd5bea028cd40
Theodor Lindgren <theodor@rabble.se>
2016-07-19 11:08:47 +0200

    Remove KVO of mapView's delegate
    Temporary fix for error in ios10 beta "UITableView was deallocated while
    key value observers were still registered with it"
    As we don't change delegate we shouldn't need it.

--- CCHMapClusterController/CCHMapViewDelegateProxy.m
+++ CCHMapClusterController/CCHMapViewDelegateProxy.m
@@ -57,21 +57,13 @@
 
 - (void)dealloc
 {
-    [self.mapView removeObserver:self forKeyPath:@"delegate"];
     self.mapView.delegate = self.target;
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-    [self.mapView removeObserver:self forKeyPath:@"delegate"];
-    [self swapDelegates];
 }
 
 - (void)swapDelegates
 {
     self.target = self.mapView.delegate;
     self.mapView.delegate = self;
-    [self.mapView addObserver:self forKeyPath:@"delegate" options:NSKeyValueObservingOptionNew context:NULL];
 }
 
 - (BOOL)respondsToSelector:(SEL)selector
